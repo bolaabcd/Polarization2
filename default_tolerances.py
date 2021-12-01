68ce3f172b67e50c1da9040a53841ff553d4f778
Artur <arthur.gaspar2011@gmail.com>
2021-12-01 15:35:26 -0300

    Added default tolerances.

---
+++ default_tolerances.py
@@ -0,0 +1,25 @@
+from enum import Enum
+import numpy as np
+
+def build_tol_graph_constant(num_agents,tolerance_value):
+    return [tolerance_value for i in range(num_agents)]
+
+def build_tol_graph_random(num_agents):
+    return np.ndarray.tolist(np.random.uniform(-1,1,num_agents))
+
+class Tolerance(Enum):
+    CONSTANT = 0
+    RANDOM = 1
+
+def build_tol_graph(
+        tol_type: Tolerance,
+        num_agents,
+        tolerance_value=None,
+        ):
+    if tol_type is Tolerance.CONSTANT:
+        if tolerance_value is None:
+            raise ValueError("Invalid tolerance value.")
+        return build_tol_graph_constant(num_agents, tolerance_value)
+    if tol_type is Tolerance.RANDOM:
+        return build_tol_graph_random(num_agents)
+    raise Exception("tol_type not recognized. Expected an `Tolerance`")
