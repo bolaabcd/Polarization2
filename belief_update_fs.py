9b2c0acfea0f236578d73966fdd3281ed9969677
Artur <arthur.gaspar2011@gmail.com>
2022-06-21 12:12:20 -0300

    Fixed interpolated update and slow_cubic, and refactored the code.

--- belief_update_fs.py
+++ belief_update_fs.py
@@ -2,11 +2,12 @@ import numpy as np
 
 # f(x,k) part of the belief-update functions 
 
-def line_update(x : float, k : float):
-    return x*k
+def line_update(x : float, k : float) -> float: # also works with numpy array.
+    y = x*k
+    return y
 
-def modulus_update(x : float, k : float):
-    k=0.5*k+0.5 # g(k)
+def modulus_update(x : float, k : float) -> float: # also works with numpy array.
+    g_k = 0.5*k + 0.5
     
     sigx = None
     if type(x) is float: 
@@ -18,45 +19,37 @@ def modulus_update(x : float, k : float):
         sigx[sigx >= 0] = 1
         sigx[sigx <  0] = -1
 
-    y = sigx*(-abs(abs(x)-k)+k)
+    y = sigx*(-abs(abs(x)-g_k)+g_k)
     return y
 
-def non_norm_quadratic_update(x : float, k : float):
-    k=k+1 # g(k)
-    y = -x*(abs(x)-k)
+def non_norm_quadratic_update(x : float, k : float): # also works with numpy array.
+    g_k = k + 1 
+    y = -x*(abs(x)-g_k)
     return y
 
-def quadratic_update(x : float, k : float):
+def quadratic_update(x : float, k : float): # also works with numpy array.
     return non_norm_quadratic_update(x, k)/2
 
-def non_norm_cubic_update(x : float, k : float):
-    k=k+1 # g(k)
-    y = -x*(x-k)*(x+k)
+def non_norm_cubic_update(x : float, k : float): # also works with numpy array.
+    g_k = k + 1
+    y = -x*(x - g_k)*(x + g_k)
     return y
 
-def cubic_update(x : float, k : float):
+def cubic_update(x : float, k : float): # also works with numpy array.
     return non_norm_cubic_update(x, k)/4
 
-def non_norm_interpolated_update(x : float, k : float):
-    y = ((k+1)*(k-1)*x**3+(-k**2+k+1)*x)
+def non_norm_interpolated_update(x : float, k : float): # also works with numpy array.
+    y = (k + 1)*(k - 1)*x**3 + (-k**2 + k + 1)*x
     return y
 
-def interpolated_update(x : float, k : float):
-    return non_norm_cubic_update(x, k)/1.5
+def interpolated_update(x : float, k : float): # also works with numpy array.
+    return non_norm_interpolated_update(x, k)/1.5
 
-def invalid_cubic(x : float, k: float):
-    # sigx = None
-    # if type(x) is float: 
-    #     sigx = -1
-    #     if x >= 0:
-    #         sigx = 1
-    # else:
-    #     sigx = np.copy(x)
-    #     sigx[sigx >= 0] = 1
-    #     sigx[sigx <  0] = -1
-    return x**3 #* sigx
+def slow_cubic(x : float, k: float): # also works with numpy array.
+    y = k*x**3
+    return y
 
-def bad_exponential(x: float, k:float):
+def bad_exponential(x: float, k:float): # also works with numpy array.
     sigx = None
     if type(x) is float: 
         sigx = -1
@@ -66,4 +59,5 @@ def bad_exponential(x: float, k:float):
         sigx = np.copy(x)
         sigx[sigx >= 0] = 1
         sigx[sigx <  0] = -1
-    return np.e**(-np.abs(x)**(-1.0))*sigx
+    y = np.e**(-np.abs(x)**(-1.0))*sigx
+    return y
