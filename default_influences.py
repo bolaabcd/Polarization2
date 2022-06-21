2d6e4070f9535d778a1d80fa21ca124ecf8d377e
Artur <arthur.gaspar2011@gmail.com>
2022-06-21 12:30:51 -0300

    Added types and reorganized imports.

--- default_influences.py
+++ default_influences.py
@@ -7,8 +7,9 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-import math
 from enum import Enum
+
+import math
 import numpy as np
 
 #######################################
@@ -56,34 +57,34 @@ MIN_INF = 0
 ## Default_Influence graphs implementation
 #####################################
 
-def build_inf_graph_clique(num_agents, influence_value, diagonal_value = 0):
+def build_inf_graph_clique(num_agents : int, influence_value : np.float64, diagonal_value : np.float64 = 0) -> np.ndarray:
     """Returns the influence graph for "clique" scenario."""
     inf_graph = np.full((num_agents, num_agents), influence_value)
-    if diagonal_value!=None:
-        np.fill_diagonal(inf_graph,diagonal_value)
+    if diagonal_value != None:
+        np.fill_diagonal(inf_graph, diagonal_value)
     return inf_graph
 
-def build_inf_graph_2_groups_disconnected(num_agents, influence_value, diagonal_value = 0):
+def build_inf_graph_2_groups_disconnected(num_agents: int, influence_value : np.float64, diagonal_value : np.float64 = 0) -> np.ndarray:
     """Returns the influence graph for for "disconnected" scenario."""
     inf_graph = np.zeros((num_agents, num_agents))
-    middle = math.ceil(num_agents / 2)
+    middle = math.ceil(num_agents/2)
     inf_graph[:middle, :middle] = influence_value
     inf_graph[middle:, middle:] = influence_value
-    if diagonal_value!=None:
-        np.fill_diagonal(inf_graph,diagonal_value)
+    if diagonal_value != None:
+        np.fill_diagonal(inf_graph, diagonal_value)
     return inf_graph
 
-def build_inf_graph_2_groups_faint(num_agents, weak_influence_value, strong_influence_value, diagonal_value = 0):
+def build_inf_graph_2_groups_faint(num_agents : int, weak_influence_value : np.float64, strong_influence_value : np.float64, diagonal_value : np.float64 = 0) -> np.ndarray:
     """Returns the influence graph for for "faintly-connected" scenario."""
     inf_graph = np.full((num_agents, num_agents), weak_influence_value)
-    middle = math.ceil(num_agents / 2)
+    middle = math.ceil(num_agents/2)
     inf_graph[:middle, :middle] = strong_influence_value
     inf_graph[middle:, middle:] = strong_influence_value
-    if diagonal_value!=None:
-        np.fill_diagonal(inf_graph,diagonal_value)
+    if diagonal_value != None:
+        np.fill_diagonal(inf_graph, diagonal_value)
     return inf_graph
 
-def build_inf_graph_2_influencers_unbalanced(num_agents, influencers_incoming_value, influencers_outgoing_value, others_influence_value, diagonal_value= 0):
+def build_inf_graph_2_influencers_unbalanced(num_agents : int, influencers_incoming_value : np.float64, influencers_outgoing_value : np.float64, others_influence_value : np.float64, diagonal_value : np.float64 = 0) -> np.ndarray:
     """Returns the influence graph for for "unbalanced 2-influencers" scenario."""
     inf_graph = np.full((num_agents, num_agents), others_influence_value)
     ## Sets the influence of agent 0 on all others
@@ -94,14 +95,14 @@ def build_inf_graph_2_influencers_unbalanced(num_agents, influencers_incoming_va
     inf_graph[1:,0] = influencers_incoming_value
     ## Sets the influence of all other agents on agent n-1.
     inf_graph[:-1, -1] = influencers_incoming_value
-    if diagonal_value!=None:
-        np.fill_diagonal(inf_graph,diagonal_value)
+    if diagonal_value != None:
+        np.fill_diagonal(inf_graph, diagonal_value)
     return inf_graph
 
 ## Note that this influence graph is not really balanced by our definition of balanced
-def build_inf_graph_2_influencers_balanced(num_agents, influencers_outgoing_value_first, influencers_outgoing_value_second, influencers_incoming_value_first, influencers_incoming_value_second, others_influence_value, diagonal_value = 0):
+def build_inf_graph_2_influencers_balanced(num_agents : int, influencers_outgoing_value_first : np.float64, influencers_outgoing_value_second : np.float64, influencers_incoming_value_first : np.float64, influencers_incoming_value_second : np.float64, others_influence_value, diagonal_value : np.float64 = 0) -> np.ndarray:
     """Returns the influence graph for for "balanced 2-influencers" scenario."""
-    inf_graph = np.full((num_agents,num_agents), others_influence_value)
+    inf_graph = np.full((num_agents, num_agents), others_influence_value)
     ## Sets the influence of agent 0 on all others
     inf_graph[0, :-1] = influencers_outgoing_value_first
     ## Sets the influence of agent n-1 on all others
@@ -110,26 +111,26 @@ def build_inf_graph_2_influencers_balanced(num_agents, influencers_outgoing_valu
     inf_graph[1:, 0] = influencers_incoming_value_first
     ## Sets the influence of all other agents on agent n-1.
     inf_graph[:-1, -1] = influencers_incoming_value_second
-    if diagonal_value!=None:
-        np.fill_diagonal(inf_graph,diagonal_value)
+    if diagonal_value != None:
+        np.fill_diagonal(inf_graph, diagonal_value)
     return inf_graph
 
-def build_inf_graph_circular(num_agents, value, diagonal_value = 0):
+def build_inf_graph_circular(num_agents : int, value : np.float64, diagonal_value : np.float64 = 0) -> np.ndarray:
     """Returns the imfluemce graph for "circular influence" scenario."""
     inf_graph = np.zeros((num_agents, num_agents))
     for i in range(num_agents):
         inf_graph[i, i] = 1.0
         inf_graph[i, (i+1) % num_agents] = value
-    if diagonal_value!=None:
-        np.fill_diagonal(inf_graph,diagonal_value)
+    if diagonal_value != None:
+        np.fill_diagonal(inf_graph, diagonal_value)
     return inf_graph
 
-def build_inf_graph_random(num_agents,diagonal_value=0,minimum_influence=MIN_INF):
-    conects=np.random.random_integers(0,1,(num_agents,num_agents))
-    rands=np.random.uniform(minimum_influence,1,(num_agents,num_agents))
-    inf_graph=conects*rands
-    if diagonal_value!=None:
-        np.fill_diagonal(inf_graph,diagonal_value)
+def build_inf_graph_random(num_agents : int, diagonal_value : np.float64 = 0, minimum_influence : np.float64 = MIN_INF) -> np.ndarray:
+    conects = np.random.random_integers(0, 1, (num_agents, num_agents))
+    rands = np.random.uniform(minimum_influence, 1, (num_agents,num_agents))
+    inf_graph = conects*rands
+    if diagonal_value != None:
+        np.fill_diagonal(inf_graph, diagonal_value)
     return inf_graph
 
 class Default_Influence(Enum):
@@ -144,16 +145,15 @@ class Default_Influence(Enum):
 def build_influence(
         inf_type: Default_Influence,
         num_agents : int,
-        *,
-        weak_influence=GROUPS_FAINTLY_INF_VALUE_WEAK,
-        strong_influence=GROUPS_FAINTLY_INF_VALUE_STRONG,
-        general_influence=None,
-        influencer_incoming_influence=None,
-        influencer_outgoing_influence=None,
-        influencer2_incoming_influence=INFLUENCERS_BALANCED_INCOMING_SECOND,
-        influencer2_outgoing_influence=INFLUENCERS_BALANCED_OUTGOING_SECOND,
-        minimum_influence = MIN_INF,
-        diagonal_value = 0):
+        weak_influence : np.float64 = GROUPS_FAINTLY_INF_VALUE_WEAK,
+        strong_influence : np.float64 = GROUPS_FAINTLY_INF_VALUE_STRONG,
+        general_influence : np.float64 = None,
+        influencer_incoming_influence : np.float64 = None,
+        influencer_outgoing_influence : np.float64 = None,
+        influencer2_incoming_influence : np.float64 = INFLUENCERS_BALANCED_INCOMING_SECOND,
+        influencer2_outgoing_influence : np.float64 = INFLUENCERS_BALANCED_OUTGOING_SECOND,
+        minimum_influence : np.float64 = MIN_INF,
+        diagonal_value : np.float64 = 0):
     """Builds the initial influence graph according to the `inf_type`.
 
     Helper function when iterating over the `Default_Influence` enum. The default values
@@ -162,13 +162,16 @@ def build_influence(
     if inf_type is Default_Influence.CLIQUE:
         if general_influence is None:
             general_influence = CLIQUE_INF_VALUE
-        return build_inf_graph_clique(num_agents, general_influence,diagonal_value=diagonal_value)
+        return build_inf_graph_clique(num_agents, general_influence, diagonal_value = diagonal_value)
+    
     if inf_type is Default_Influence.GROUP_2_DISCONECTED:
         if general_influence is None:
             general_influence = GROUPS_DISCONNECTED_INF_VALUE
-        return build_inf_graph_2_groups_disconnected(num_agents, general_influence,diagonal_value=diagonal_value)
+        return build_inf_graph_2_groups_disconnected(num_agents, general_influence, diagonal_value = diagonal_value)
+    
     if inf_type is Default_Influence.GROUP_2_FAINT:
-        return build_inf_graph_2_groups_faint(num_agents, weak_influence, strong_influence,diagonal_value=diagonal_value)
+        return build_inf_graph_2_groups_faint(num_agents, weak_influence, strong_influence, diagonal_value = diagonal_value)
+    
     if inf_type is Default_Influence.INFLUENCERS_2_UNBALANCED:
         if general_influence is None:
             general_influence = INFLUENCERS_UNBALANCED_OTHERS
@@ -176,9 +179,9 @@ def build_influence(
             influencer_incoming_influence = INFLUENCERS_UNBALANCED_INCOMING_BOTH
         if influencer_outgoing_influence is None:
             influencer_outgoing_influence = INFLUENCERS_UNBALANCED_OUTGOING_BOTH
-        return build_inf_graph_2_influencers_unbalanced(num_agents, influencer_incoming_influence, influencer_outgoing_influence, general_influence,diagonal_value=diagonal_value)
+        return build_inf_graph_2_influencers_unbalanced(num_agents, influencer_incoming_influence, influencer_outgoing_influence, general_influence, diagonal_value = diagonal_value)
 
-## Note that this influence graph is not really balanced by our definition of balanced
+    ## Note that this influence graph is not really balanced by our definition of balanced
     if inf_type is Default_Influence.INFLUENCERS_2_BALANCED:
         if general_influence is None:
             general_influence = INFLUENCERS_BALANCED_OTHERS
@@ -190,15 +193,18 @@ def build_influence(
             influencer_outgoing_influence = INFLUENCERS_BALANCED_OUTGOING_FIRST
         if influencer2_outgoing_influence is None:
             influencer2_outgoing_influence = INFLUENCERS_BALANCED_OUTGOING_SECOND
-        return build_inf_graph_2_influencers_balanced(num_agents, influencer_outgoing_influence, influencer2_outgoing_influence, influencer_incoming_influence, influencer2_incoming_influence, general_influence,diagonal_value=diagonal_value)
+        return build_inf_graph_2_influencers_balanced(num_agents, influencer_outgoing_influence, influencer2_outgoing_influence, influencer_incoming_influence, influencer2_incoming_influence, general_influence, diagonal_value = diagonal_value)
+    
     if inf_type is Default_Influence.CIRCULAR:
         if general_influence is None:
             general_influence = CIRCULAR_INF_VALUE
-        return build_inf_graph_circular(num_agents, general_influence,diagonal_value=diagonal_value) 
+        return build_inf_graph_circular(num_agents, general_influence, diagonal_value = diagonal_value) 
+    
     if inf_type is Default_Influence.RANDOM:
         return build_inf_graph_random(
             num_agents,
             diagonal_value = diagonal_value,
             minimum_influence = minimum_influence
             )
+    
     raise NotImplementedError('inf_type not recognized. Expected an `Default_Influence`')
