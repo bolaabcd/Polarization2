fde86fa90b4287f76633b5737f481d7c0c8779dd
Artur <arthur.gaspar2011@gmail.com>
2022-06-21 12:34:39 -0300

    Added types and reorganized imports.

--- default_tolerances.py
+++ default_tolerances.py
@@ -1,29 +1,25 @@
 from enum import Enum
 import numpy as np
 
-def build_tol_list_constant(num_agents, in_tolerance, out_tolerance):
-    return [(in_tolerance, out_tolerance) for i in range(num_agents)]
+def build_tol_matrix_constant(num_agents : int, tolerance_value : np.float64) -> np.ndarray:
+    return np.full((num_agents, num_agents), tolerance_value)
 
-def build_tol_list_random(num_agents):
-    rlist = np.ndarray.tolist(np.random.uniform(-1,1,2*num_agents))
-    return zip(rlist[::2], rlist[1::2])
-
+def build_tol_matrix_random(num_agents : int) -> np.ndarray:
+    return np.random.uniform(-1, 1, (num_agents, num_agents))
+    
 class Tolerance(Enum):
     CONSTANT = 0
     RANDOM = 1
 
 def build_tol_list(
         tol_type: Tolerance,
-        num_agents,
-        in_tolerance = None,
-        out_tolerance = None
+        num_agents: int,
+        tol_val : np.float64 = 1
         ):
+    if num_agents < 0:
+        raise ValueError('Invalid number of agents.')
     if tol_type is Tolerance.CONSTANT:
-        if in_tolerance is None:
-            raise ValueError("Invalid tolerance value.")
-        elif out_tolerance is None:
-            out_tolerance = in_tolerance
-        return build_tol_list_constant(num_agents, in_tolerance, out_tolerance)
+        return build_tol_list_constant(num_agents, tol_val)
     if tol_type is Tolerance.RANDOM:
         return build_tol_list_random(num_agents)
-    raise Exception("tol_type not recognized. Expected an `Tolerance`")
+    raise ValueError('tol_type not recognized. Expected a "Tolerance"')
