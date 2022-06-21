34473567feec4002ac3f40478ca3964b02f5ae37
Artur <arthur.gaspar2011@gmail.com>
2022-06-21 12:31:12 -0300

    Added types and reorganized imports.

--- default_fs.py
+++ default_fs.py
@@ -1,5 +1,5 @@
 from types import FunctionType
+import numpy as np
 
-
-def same(num_agents : int, f : FunctionType):
-    return [f for i in range(num_agents)]
+def same(num_agents : int, f : FunctionType) -> np.ndarray:
+    return np.full(num_agents, f)
